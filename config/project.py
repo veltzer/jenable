abee44953d3d612eaad5849dd8a6df336ae243d6
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:00 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "jenable"
-project_long_description = "macro processor for Java"
-project_year_started = 2010
-project_description = """macro processor for Java"""
+description_short = "macro processor for Java"
